c059b5c045885a9db3351d5e15548d629b7e885d
kashafashraf230 <kashafashraf412@gmail.com>
2023-04-16 12:34:57 +0500

    Showing BMI on Dialog

--- app/src/main/java/com/example/bmi_calculator/MainActivity.java
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.bmi_calculator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.bmi_calculator.core.BMICalculator;
 
@@ -23,32 +26,55 @@ public class MainActivity extends AppCompatActivity {
         userHeight.findViewById(R.id.userHeight);
         button.findViewById(R.id.resultButton);
 
+    }
+
+    public void buttonClick(View v){
+
         String weightText = userWeight.getText().toString();
         String heightText = userHeight.getText().toString();
 
+        Double weight = 0.00;
+        Double height = 0.00;
 
-        button.setOnClickListener(new View.OnClickListener(){
+        if(!weightText.equals("") && Double.parseDouble(weightText) != 0){
+            weight = Double.parseDouble(weightText);
+        }
 
-            public void onClick(View v){
+        if(!heightText.equals("") && Double.parseDouble(heightText) != 0){
+            height = Double.parseDouble(heightText);
+        }
 
-                Double weight = 0.00;
-                Double height = 0.00;
+        BMICalculator bmiCalculator = new BMICalculator();
 
-                if(!weightText.equals("") && Double.parseDouble(weightText) != 0){
-                   weight = Double.parseDouble(weightText);
+        Double bmiResult =bmiCalculator.calculateBMI(weight, height);
+
+        String resultNote;
+
+        if(bmiResult != -1){
+
+            String resultCategory = bmiCalculator.getCategory(bmiResult);
+            resultNote = "Your BMI index is"+ String.format("%.2f", bmiResult)+ "\nBMI Category"+ resultCategory;
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("BMI");
+            alert.setMessage(resultNote);
+            alert.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    userWeight.setText("");
+                    userHeight.setText("");
                 }
+            });
 
-                if(!heightText.equals("") && Double.parseDouble(heightText) != 0){
-                    height = Double.parseDouble(heightText);
-                }
 
-                BMICalculator bmiCalculator = new BMICalculator();
+            AlertDialog alertDialog = alert.create();
+            alertDialog.show();
 
-                Double bmiResult =bmiCalculator.calculateBMI(weight, height);
+        }
 
-            }
-
-        });
+        else{
+            Toast.makeText(this, bmiCalculator.getErrorMsg(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
